2a72a1848fbe508e6b0612e06d627225648f5e3d
badri7489 <59701113+badri7489@users.noreply.github.com>
2020-07-19 21:20:50 +0530

    Minor Bugs fixed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -76,7 +76,7 @@ def comment_remove(request, pk):
     return redirect('post_detail', pk = post_pk)
 
 @login_required
-def post_pubish(request, pk):
+def post_publish(request, pk):
     post = get_object_or_404(Post, pk = pk)
-    post.publish
+    post.publish()
     return redirect('post_detail', pk = pk)
